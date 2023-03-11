a90175a9dd499f4db80698d069bbefbd43a773b1
LeBogo <lebogomc@gmail.com>
2023-03-11 12:30:12 +0100

    add button for downloading log

--- src/DevMode.ts
+++ src/DevMode.ts
@@ -1,5 +1,6 @@
 export class DevMode {
     public static instance: DevMode;
+    private static devLog: string[] = [];
     active: boolean = false;
     devModeToggle: HTMLDivElement = document.querySelector("#devToggle")!;
     devModeContainer: HTMLDivElement = document.querySelector("#debugOutput")!;
@@ -9,6 +10,24 @@ export class DevMode {
         DevMode.instance = this;
         this.devModeToggle.addEventListener("dblclick", () => {
             this.toggle();
+        });
+
+        document.querySelector("#downloadLog")!.addEventListener("click", () => {
+            const log = DevMode.devLog.join("\n");
+
+            var element = document.createElement("a");
+            element.setAttribute(
+                "href",
+                "data:text/plain;charset=utf-8," + encodeURIComponent(log)
+            );
+            const now = new Date();
+            element.setAttribute(
+                "download",
+                `onu-log-${now.toLocaleDateString()}-${now.toLocaleTimeString()}.log`
+            );
+
+            element.style.display = "none";
+            element.click();
         });
     }
 
@@ -28,8 +47,11 @@ export class DevMode {
     }
 
     static log(emoji: string, message: string) {
+        const logEntry = `${new Date().toLocaleTimeString()} ${emoji} ${message}`;
+        this.devLog.push(logEntry);
         const li = document.createElement("li");
-        li.innerHTML = `${emoji} ${message}`;
+        li.innerHTML = logEntry;
+
         // insert new log at the top
         DevMode.instance.debugLog.insertBefore(li, DevMode.instance.debugLog.firstChild);
         // DevMode.instance.debugLog.appendChild(li);
